810a93161fc00ef33600e589646821cec7696f6d
nutty <>
2024-01-25 21:56:41 +0700

    page 2 edit padding

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,14 +16,15 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,26 +33,29 @@ import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Qualify2Screen() {
     Scaffold(
         containerColor = Color.White,
         topBar = {
             Box(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp),
                 contentAlignment = Alignment.TopEnd
             ) {
-                Button(
-                    modifier = Modifier.padding(top = 4.dp),
-                    onClick = { },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Transparent,
-                        contentColor = MaterialTheme.colorScheme.primary,
-                    ),
-                ) {
+                Button(modifier = Modifier
+                    .padding(8.dp)
+                    .width(60.dp)
+                    .height(40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                    contentPadding = PaddingValues(0.dp), onClick = { /*TODO*/ }) {
                     Text(
+                        modifier = Modifier,
                         text = "Skip",
                         style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.primary,
                     )
                 }
             }
@@ -59,7 +64,7 @@ fun Qualify2Screen() {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(100.dp)
+                    .height(102.dp)
                     .background(MaterialTheme.colorScheme.primaryContainer),
                 contentAlignment = Alignment.Center
             ) {
@@ -77,59 +82,70 @@ fun Qualify2Screen() {
         Box(
             modifier = Modifier
                 .padding(innerPadding)
-        ) {}
-    }
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-    ) {
-        Column(
-            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
                 .padding(horizontal = 32.dp)
-                .padding(top = 68.dp),
-            verticalArrangement = Arrangement.spacedBy(32.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-                // Title
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    text = "Lorem ipsum dolor sit amet",
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    textAlign = TextAlign.Center
-                )
-                // Description
-                Text(
-                    modifier = Modifier.height(120.dp),
-                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing\n elit. " +
-                            "Donec egestas dictum lacinia. Integer arcu\n neque, porttitor ac metus quis, " +
-                            "iaculis molestie\n magna. Vivamus molestie justo sed nulla lacinia, " +
-                            "quis\n fringilla lorem imperdiet. Proin in quam vel odio iaculis fringilla",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    textAlign = TextAlign.Center,
-                )
-            }
-            // Image
-            Image(
+            TextDisplay()
+            ImageDisplay(
                 modifier = Modifier
-                    .width(347.dp).height(446.dp)
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(MaterialTheme.colorScheme.inverseSurface.copy(alpha = 0.5f)),
-                painter = painterResource(id = R.drawable.img_qualify_2_onboard),
-                contentDescription = ""
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 96.dp)
             )
-            ImageIndicator()
+            ImageIndicator(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 48.dp)
+            )
         }
     }
 }
 
 @Composable
-fun ImageIndicator() {
+fun TextDisplay() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Lorem ipsum dolor sit amet",
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = TextAlign.Center
+        )
+        // Description
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp),
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing\n elit. " +
+                    "Donec egestas dictum lacinia. Integer arcu\n neque, porttitor ac metus quis, " +
+                    "iaculis molestie\n magna. Vivamus molestie justo sed nulla lacinia, \n" +
+                    "quis fringilla lorem imperdiet. Proin in quam vel odio iaculis fringilla",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun ImageDisplay(modifier: Modifier) {
+    // Image
+    Image(
+        modifier = modifier
+            .height(448.dp),
+        contentScale = ContentScale.FillBounds,
+        painter = painterResource(id = R.drawable.img_qualify_2_onboard),
+        contentDescription = ""
+    )
+}
+
+@Composable
+fun ImageIndicator(modifier: Modifier) {
     Box(
-        modifier = Modifier,
+        modifier = modifier,
         contentAlignment = Alignment.Center
     ) {
         Row(
